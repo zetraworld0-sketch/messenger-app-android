c54f6e035a1796185b08c8c5248bfcf63344631e
zetraworld0-sketch <zetraworld0@gmail.com>
2026-05-13 22:30:43 +0100

    Create CustomLoginActivity.java

---
+++ app/src/main/java/org/mesibo/messenger/CustomLoginActivity.java
@@ -0,0 +1,13 @@
+package org.mesibo.messenger;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CustomLoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // We will create this layout later
+        setContentView(R.layout.activity_custom_login); 
+    }
+}
