700e88c3d0edf171bcdb60d33d346dbe61ea01b6
zetraworld0-sketch <zetraworld0@gmail.com>
2026-05-14 07:28:09 +0100

    Update MainActivity.java

--- app/src/main/java/org/mesibo/messenger/MainActivity.java
+++ app/src/main/java/org/mesibo/messenger/MainActivity.java
@@ -1,12 +1,25 @@
 package org.mesibo.messenger;
+
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * Zetra Lab - Netscape Sovereign Hub
+ * This version completely ignores the default Mesibo UI logic.
+ */
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // This will point to your hub's main dashboard later
+        
+        // This line links to the XML you just showed me.
+        // By NOT calling Mesibo UI functions here, the old screens can't load.
         setContentView(R.layout.activity_main); 
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Institutional-grade security: prevent going back to Login without intent
+        super.onBackPressed();
     }
 }
