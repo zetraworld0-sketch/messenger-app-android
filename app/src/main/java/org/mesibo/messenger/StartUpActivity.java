75e5c059c3f2b47e98a1c9a57618f504dd87a529
zetraworld0-sketch <zetraworld0@gmail.com>
2026-05-14 07:35:36 +0100

    Update StartUpActivity.javaM

--- app/src/main/java/org/mesibo/messenger/StartUpActivity.java
+++ app/src/main/java/org/mesibo/messenger/StartUpActivity.java
@@ -1,125 +1,43 @@
 /******************************************************************************
-* By accessing or copying this work, you agree to comply with the following   *
-* terms:                                                                      *
-*                                                                             *
-* Copyright (c) 2019-2024 mesibo                                              *
-* https://mesibo.com                                                          *
-* All rights reserved.                                                        *
-*                                                                             *
-* Redistribution is not permitted. Use of this software is subject to the     *
-* conditions specified at https://mesibo.com . When using the source code,    *
-* maintain the copyright notice, conditions, disclaimer, and  links to mesibo * 
-* website, documentation and the source code repository.                      *
-*                                                                             *
-* Do not use the name of mesibo or its contributors to endorse products from  *
-* this software without prior written permission.                             *
-*                                                                             *
-* This software is provided "as is" without warranties. mesibo and its        *
-* contributors are not liable for any damages arising from its use.           *
-*                                                                             *
-* Documentation: https://docs.mesibo.com/                                     *
-*                                                                             *
-* Source Code Repository: https://github.com/mesibo/                          *
+* ZETRA LABS - NETSCAPE SOVEREIGN HUB PURGE PROTOCOL
+* This file has been hollowed out to prevent legacy UI injection.
 *******************************************************************************/
 
 package org.mesibo.messenger;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentSender;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
 
-import com.mesibo.api.Mesibo;
-import com.mesibo.messaging.MesiboUI;
-import com.mesibo.uihelper.MesiboUiHelperConfig;
-
+/**
+ * STARTUP ACTIVITY (NEUTRALIZED)
+ * 
+ * In the original Mesibo source, this class was responsible for launching 
+ * the default User List, Settings, and Welcome screens. 
+ * 
+ * To maintain an institutional-grade build, all UIManager and MesiboUI 
+ * references have been stripped. This ensures that the ghost of the 
+ * original app cannot reappear after the Custom Login.
+ */
 public class StartUpActivity extends AppCompatActivity {
-
-    private static final String TAG = "MesiboStartupActivity";
-    public final static String INTENTEXIT="exit";
-    public final static String SKIPTOUR="skipTour";
-    public final static String STARTINBACKGROUND ="startinbackground";
-    private boolean mRunInBackground = false;
-    private boolean mPermissionAlert = false;
-
-    public static void newInstance(Context context, boolean startInBackground) {
-        Intent i = new Intent(context, StartUpActivity.class);  //MyActivity can be anything which you want to start on bootup...
-        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        //i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        i.putExtra(StartUpActivity.STARTINBACKGROUND, startInBackground);
-
-        context.startActivity(i);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(getIntent().getBooleanExtra(INTENTEXIT, false)) {
-            Log.d(TAG, "onCreate closing");
-            finish();
-            return;
-        }
-
-
-
-        mRunInBackground = getIntent().getBooleanExtra(STARTINBACKGROUND, false);
-
-        long elapsed = Mesibo.getTimestamp() - Mesibo.getStartTimestamp();
-
-        Log.e(TAG, "Elapsed " + elapsed + ", " + (mRunInBackground?"background":"foreground"));
-        if(elapsed > 30000)
-            mRunInBackground = false;
-
-        if(mRunInBackground) {
-            Log.e(TAG, "Moving app to background");
-            moveTaskToBack(true);
-        } else {
-            Log.e(TAG, "Not Moving app to background");
-        }
-
-
-        setContentView(R.layout.activity_blank_launcher);
-        startNextActivity();
+        /* 
+         * SCIENTIST NOTE: We call finish() immediately. 
+         * If any background process or SDK listener attempts to trigger 
+         * this Activity, it will close instantly without drawing 
+         * a single pixel of the "legacy" interface.
+         */
+        finish(); 
     }
 
-    void startNextActivity() {
-
-        if(TextUtils.isEmpty(SampleAPI.getToken())) {
-            MesiboUiHelperConfig.mDefaultCountry = 1;
-            MesiboUiHelperConfig.mPhoneVerificationBottomText = "Note, Mesibo may call instead of sending an SMS if SMS delivery to your phone fails.";
-
-
-            if(getIntent().getBooleanExtra(SKIPTOUR, false)) {
-                UIManager.launchLogin(this, MesiboListeners.getInstance());
-            } else {
-                UIManager.launchWelcomeactivity(this, true, MesiboListeners.getInstance(), MesiboListeners.getInstance());
-            }
-
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-
-        } else {
-            MesiboUI.MesiboUserListScreenOptions opts = new MesiboUI.MesiboUserListScreenOptions();
-            opts.keepRunning = true;
-            opts.startInBackground = mRunInBackground;
-            UIManager.launchMesibo(this, opts);
-        }
-
-        finish();
-    }
-
-    // since this activity is singleTask, intent will be delivered here if it's running
-    @Override
-    protected void onNewIntent(Intent intent) {
-        Log.d(TAG, "onNewIntent");
-        if(intent.getBooleanExtra(INTENTEXIT, false)) {
-            finish();
-        }
-
-        super.onNewIntent(intent);
+    /**
+     * Legacy entry point kept only for compilation stability.
+     * It no longer performs any background initialization.
+     */
+    public static void newInstance(android.content.Context context, boolean startInBackground) {
+        // Purged: No longer allows the app to start the old logic in background.
     }
 }
